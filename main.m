5eb8e58aeb65d0103b5e52d6523cb9e38ab44581
Str4wbeary <xcai30@ucsc.edu>
2022-05-26 16:54:41 -0700

    Update main.m to unzip and delete images

--- main.m
+++ main.m
@@ -1,1 +1,9 @@
-unzip("Fri_May_27_01-39-58_CEST_2022.zip");
+% unzip the images and store them in corresponding folders
+mkdir("neutral");
+mkdir("angry");
+unzip("neutral.zip", "neutral");
+unzip("angry.zip", "angry");
+
+% remove and delete all images and folders created for the script
+rmdir("angry", 's');
+rmdir("neutral", 's');
