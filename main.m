102e4f3b8a15e62173f2540ddb575c4c2e922731
Str4wbeary <xcai30@ucsc.edu>
2022-05-26 17:00:08 -0700

    unzip and delete work on root directory

--- main.m
+++ main.m
@@ -1,9 +1,9 @@
 % unzip the images and store them in corresponding folders
-mkdir("neutral");
-mkdir("angry");
-unzip("neutral.zip", "neutral");
-unzip("angry.zip", "angry");
+mkdir("data\faces\neutral");
+mkdir("data\faces\angry");
+unzip("data\faces\neutral.zip", "data\faces\neutral");
+unzip("data\faces\angry.zip", "data\faces\angry");
 
 % remove and delete all images and folders created for the script
-rmdir("angry", 's');
-rmdir("neutral", 's');
+rmdir("data\faces\angry", 's');
+rmdir("data\faces\neutral", 's');
