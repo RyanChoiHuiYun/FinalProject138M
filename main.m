6848ab210da44406ad35c2fe70c9ead14db4cf8b
Str4wbeary <xcai30@ucsc.edu>
2022-05-27 17:58:36 -0700

    Updated main.m
    Showing images and words properly. TODO: analysis.

--- main.m
+++ main.m
@@ -1,3 +1,7 @@
+% Ryan Cai
+% xcai30@ucsc.edu
+
+% PSYC 138P Final Project: Lexical Decision Task with Primed Faces
 
 % start of region
 % ---------------
@@ -6,12 +10,15 @@
 %%%%%%%%%%%%%%%
 % STIMULI - images
 % unzip the images and store them in corresponding folders
+fprintf("Loading necessary stimuli and creating folders...\n");
 mkdir("stimuli\faces\neutral");
 addpath("stimuli\faces\neutral");
 mkdir("stimuli\faces\angry");
 addpath("stimuli\faces\angry");
 unzip("stimuli\faces\neutral.zip", "stimuli\faces\neutral");
 unzip("stimuli\faces\angry.zip", "stimuli\faces\angry");
+fprintf("\nDone.\n");
+pause(0.5);
 % randomize choice from neutral or angry
 s = 1:24;
 s = s(randperm(length(s)));
@@ -44,7 +51,16 @@ nonwordlist_r = nonwordlist(randperm(12));
 %%%%%%%%%%%%%%%%
 initials = input('Please enter your initials: ', 's'); 
 output_file = ['lexical_data_' initials '.mat' ];
-% TODO
+pause(0.5);
+clc;
+fprintf("Welcome to the lexical decision task with primed faces." + ...
+    "\nIn each trial, a random neutral or angry face will be briefly shown " + ...
+    "before prompting a word or a non-word for you to answer." + ...
+    "\nPlease press F for words, J for non-words." + ...
+    "\nMake your response as fast and as accurate as you can.\n" + ...
+    "Good luck!\n");
+pause(0.5);
+input("Please press enter to start...");
 % end of region
 % -------------
 
@@ -57,13 +73,13 @@ output_file = ['lexical_data_' initials '.mat' ];
 word_counter = 1;
 nonword_counter = 1;
 % matrix to collect data
-trial_matrix = [];
+trial_matrix = zeros(24, 6);
 column_names = {'trial_num', 'cond_face', 'cond_word', 'response', 'rt', 'accuracy'};
 % 24 trials in total
 for i = 1:24
     % neutral
     if s(i) <= 12
-        disp("displaying face...");
+        cond_face = 0;
         figure;
         img = imread(n(1).name);
         imshow(img);
@@ -72,9 +88,11 @@ for i = 1:24
         n(1) = [];
         % words
         if k(i) <= 12
+            cond_word = 1;
             str = wordlist_r{word_counter};
             word_counter = word_counter + 1;
         else
+            cond_word = 0;
             str = nonwordlist_r{nonword_counter};
             nonword_counter = nonword_counter + 1;
         end
@@ -86,9 +104,9 @@ for i = 1:24
         while valid_key == 0
             [x, y, b] = ginput(1);
             if b == 102
-                resp = 1; %F = word = 1
+                resp = 1; % F = word = 1
                 valid_key = 1;
-            elseif b == 106 %J = nonword = 0
+            elseif b == 106 % J = nonword = 0
                 resp = 0;
                 valid_key = 1;
             end        
@@ -98,7 +116,7 @@ for i = 1:24
     % angry
     else
         % displaying the face
-        disp("displaying face...");
+        cond_face = 1;
         figure;
         img = imread(a(1).name);
         imshow(img);
@@ -107,9 +125,11 @@ for i = 1:24
         a(1) = [];
         % words
         if k(i) <= 12
+            cond_word = 1;
             str = wordlist_r{word_counter};
             word_counter = word_counter + 1;
         else
+            cond_word = 0;
             str = nonwordlist_r{nonword_counter};
             nonword_counter = nonword_counter + 1;
         end
@@ -121,9 +141,9 @@ for i = 1:24
         while valid_key == 0
             [x, y, b] = ginput(1);
             if b == 102
-                resp = 1; %F = word = 1
+                resp = 1; % F = word = 1
                 valid_key = 1;
-            elseif b == 106 %J = nonword = 0
+            elseif b == 106 % J = nonword = 0
                 resp = 0;
                 valid_key = 1;
             end        
@@ -132,10 +152,42 @@ for i = 1:24
         delete(t1);
     end
     % Collect data and save them in trial_matrix
-    % TODO
+    trial_matrix(i, 1) = i;
+    trial_matrix(i, 2) = cond_face;
+    trial_matrix(i, 3) = cond_word;
+    trial_matrix(i, 4) = resp;
+    trial_matrix(i, 5) = rt;
+    trial_matrix(i, 6) = cond_word == resp;
+    % Save output to the file
+    save(output_file, "trial_matrix", "column_names");
+    pause(1);
+
 end
+close("all");
+fprintf("Thank you for your participation.\nGoodbye!");
+% end of region
+% -------------
 
+% start of region
+% ---------------
+%%%%%%%%%%%%
+% Analysis %
+%%%%%%%%%%%%
+% load(output_file);
+% TODO
+%
+% end of region
+% -------------
 
+% start of region
+% ---------------
+%%%%%%%%%%%%%%
+% Conclusion %
+%%%%%%%%%%%%%%
 % remove and delete all images and folders created for the script
+fprintf("Deleting necessary stimuli and folders...\n");
 rmdir("stimuli\faces\angry", 's');
 rmdir("stimuli\faces\neutral", 's');
+fprintf("\nDone.\n");
+% end of region
+% -------------
