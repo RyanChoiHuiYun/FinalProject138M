31c9174146c2b4ef15bf721fc2698913ef6f54f4
JTMeltonUCSC <74630172+JTMeltonUCSC@users.noreply.github.com>
2022-06-03 16:22:05 -0700

    Update analysis.m

--- analysis.m
+++ analysis.m
@@ -2,15 +2,11 @@
 fnames = dir("output/*.mat");
 for i = 1:length(fnames)
     load("output/"+fnames(i).name);
-  
 
     %Computing the mean response time for each condition
     meanRT_cond1(i) = mean(trial_matrix(trial_matrix(:,2)==1,3));
     meanRT_cond0(i) = mean(trial_matrix(trial_matrix(:,2)==0,3));
 end
-
-
-%Computing the mean response time for each condition
 
 %Runnning and outputting the results of a two-sample ttest
 [h,p,ci,stat] = ttest(meanRT_cond1, meanRT_cond0)
